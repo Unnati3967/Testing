c33c5b4a310f35e38844db1e26acc18d514f93c4
Unnati3967 <unnatipande3967@gmail.com>
2024-07-10 10:17:42 +0530

    Update DemoGit.java

--- GitProject/src/com/cjc/git/DemoGit.java
+++ GitProject/src/com/cjc/git/DemoGit.java
@@ -3,5 +3,6 @@ package com.cjc.git;
 public class DemoGit {
 public static void main(String[] args) {
 	System.out.println("This is git repository");
+	System.out.println("Already commited and ready to pull");
 }
 }
